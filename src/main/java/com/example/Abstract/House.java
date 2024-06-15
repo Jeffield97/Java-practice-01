698cf229c9eca975588fb6f8a1c77a613aa01610
Jeffield97 <jchariguaman@gmail.com>
2024-06-15 12:21:14 -0500

    Add: House class, using builder pattern

---
+++ src/main/java/com/example/Abstract/House.java
@@ -0,0 +1,55 @@
+package com.example.Abstract;
+
+public class House {
+    Double area;
+    Double cost;
+    String owner;
+
+    public House(Builder builder) {
+        this.area = builder.area;
+        this.cost = builder.cost;
+        this.owner = builder.owner;
+    }
+    
+
+    public Double getArea() {
+        return area;
+    }
+
+
+    public Double getCost() {
+        return cost;
+    }
+
+
+    public String getOwner() {
+        return owner;
+    }
+
+
+    public static class Builder {
+        private Double area;
+        private Double cost;
+        private String owner;
+
+        public Builder area(Double area) {
+            this.area = area;
+            return this;
+        }
+
+        public Builder cost(Double cost) {
+            this.cost = cost;
+            return this;
+        }
+
+        public Builder owner(String owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public House build() {
+            return new House(this);
+        }
+    }
+
+}
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.io.PrintStream;
 
+import com.example.Abstract.House;
 import com.example.Abstract.Impl.Employee;
 
 /**
@@ -10,10 +11,19 @@ import com.example.Abstract.Impl.Employee;
  */
 public class App {
     public static void main(String[] args) {
-       Employee employee = new Employee();
-       employee.setName("Jeffield");
-       employee.setAge(20);
-       employee.setSalary(900.0);
+        Employee employee = new Employee();
+        employee.setName("Jeffield");
+        employee.setAge(20);
+        employee.setSalary(900.0);
+
+        // Call to buildHouse
+        House house = buildHouse();
+        System.out.println(house.getOwner());
     }
 
+    //Builder patron
+    public static House buildHouse() {
+        House house = new House.Builder().area(200.0).cost(300.0).owner("Jeffield").build();
+        return house;
+    }
 }
